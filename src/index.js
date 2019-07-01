e0f68f70f9544395bcaa8f014a7bafaf8dc67229
bigyo.dev <bigyo.dev@gmail.com>
2019-07-01 17:33:18 +0900

    commit~

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'ㅅ니기니ㅏㅎ??';
+
+ReactDOM.render(
+  <div>{title}{title}</div>,
+  document.getElementById('root')
+);
+
+module.hot.accept();
