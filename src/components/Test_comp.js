25207e4316c9a96c3386f11249c5318e5490168d
bigyo-dev <bigyo.dev@gmail.com>
2019-07-02 10:13:56 +0900

    add compoents!

---
+++ src/components/Test_comp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Test_comp extends Component {
+  render(){
+    return(
+      <div>
+        <h1>test component</h1>
+        <p>this is a test component</p>
+      </div>
+    )
+  }
+}
+
+export default Test_comp;
